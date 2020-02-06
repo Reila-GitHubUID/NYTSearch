1b88d40f03095a9e934b3a0029f3cbeb961c91fe
Ellin Tj <tjellin@gmail.com>
2020-02-05 23:14:32 -0800

    added ajax lines

--- NYTSearch/assets/back.js
+++ NYTSearch/assets/back.js
@@ -1,2 +1,9 @@
 let apiKey = "";
 let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + question + "&api-key=" + apiKey;
+
+$.ajax ({
+    url,
+    method: "GET"
+}).then (function(response) {
+    console.get(response);
+});
